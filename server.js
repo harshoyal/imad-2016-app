20c3ea5f110cb1d5e8d3bad0adec81c2fe0ec8d4
harshoyal <harshoyal@gmail.com>
2016-09-29 20:01:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("its first page example");
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
