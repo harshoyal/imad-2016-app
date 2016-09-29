9376b3041436fbbebec6905da05191986d327098
harshoyal <harshoyal@gmail.com>
2016-09-30 08:10:18 +1200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,11 +9,11 @@ element.innerHTML = "new value is just un certain so kindly wait";
 
 var img = document.getElementById("madi")
 var marginLeft=0;
+
 function moveRight ()
 {
     marginLeft= marginLeft + 1 ;
     img.style.marginLeft= marginLeft + 'px';
-    
 }
 
 img.onclick = function() {
