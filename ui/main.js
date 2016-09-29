008392a94dafb57cbdf4b3ba40672cd9af687d65
harshoyal <harshoyal@gmail.com>
2016-09-30 04:20:29 +1200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
 console.log('Loaded!');
+//change the text of the main page and see it happening
+
+var element = document.getElementById("1");
+
+element.innerHtml = "new value is just un certain so kindly wait";
