66a0cb336a0c75fc21a1fc033ed59c3b7adf50e6
harshoyal <harshoyal@gmail.com>
2016-09-30 04:55:30 +1200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,12 +16,12 @@ function moveRight ()
     
 }
 
-img.onclick = function(){
+img.onclick = function() {
+
+var interval= setInterval(moveRight,100);
 //before
 //img.style.marginLeft='100px';
 
     //after
     // haer moveRight() is the function on every 100 ms 
-var interval= setInterval(moveRight,100);
-
 };
